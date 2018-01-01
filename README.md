79b2b31ee2bf5d4e50ca4827e7c7d63822dad920
Alejandro Molina <jalemolina@gmail.com>
2018-01-01 15:54:34 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,34 @@
+.cmus
+=====
+
+
+Mi configuración de cmus.
+
+Instalación
+-----------
+
+Cómo se instala:
+
+Primero hay que clonar el repo especificando el directorio destino `~/.cmus`.
+
+`git clone git@github.com:jalemolina/cmus.git ~/.cmus`
+
+y listo.
+
+¡A escuchar música!
+
+
+Plugins
+-------
+
+TODO
+
+Esquema de colores
+------------------
+
+TODO
+
+Machete
+-------
+
+[cheatsheet](./cheatsheet.md)
---
+++ lib.pl
@@ -0,0 +1,247 @@
+/home/ale/Música/2'/Amor suicida.mp3
+/home/ale/Música/2'/Arrebato.mp3
+/home/ale/Música/2'/Cancion de amor.mp3
+/home/ale/Música/2'/Demasiado tarde.mp3
+/home/ale/Música/2'/dos minutos - advertencia - lejos estoy.mp3
+/home/ale/Música/2'/dos minutos - antidoping lola.mp3
+/home/ale/Música/2'/dos minutos - cerveza.mp3
+/home/ale/Música/2'/dos minutos - pobre susana.mp3
+/home/ale/Música/2'/dos minutos - ska de la rutina.mp3
+/home/ale/Música/2'/Que mala suerte.mp3
+/home/ale/Música/2'/Vos no confiaste.mp3
+/home/ale/Música/2'/Ya no sos igual.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/01 - Valentín Alsina.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/02 - Canción de amor.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/03 - !Que mala suerte!.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/04 - Novedades.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/05 - Odio laburar.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/06 - Amor suicida.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/07 - 14 botellas.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/08 - Vos no confiaste.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/09 - Barricada.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/10 - Otra mujer.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/11 - Como caramelo de limón.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/12 - Demasiado tarde (La marcha).mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/13 - Pelea callejera.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/14 - Arrebato.mp3
+/home/ale/Música/2'/1994 - 2 Minutos - Valentín Alsina/15 - Ya no sos igual.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/01 Copetin al paso.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/02 Todo lo miro.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/03 Ataque.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/05 Borracho y agresivo.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/06 Laburantes.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/08 El mejor recuerdo.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/09 Vago.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/11 Pi as Van, Pi as Vienen.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/12 Q E P D.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/07 Mosca de bar.mp3
+/home/ale/Música/2'/1995 - Volvio la alegria, vieja!!!/10 Otra Vez la casa de Juan.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/01 - Qué yeta.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/02 - 1987.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/03 - El se va.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/04 - No mientas más.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/05 - La balanza.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/06 - Mal romance.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/07 - Recuerdos en la arena.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/08 - A veces.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/09 - Los Chuquis.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/10 - La ladrona.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/11 - Domingos.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/12 - Postal '95.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/13 - Gatillo fácil.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/14 - Los ladrones.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/15 - Nada que hacer.mp3
+/home/ale/Música/2'/1997 - 2 Minutos - Postal ´97/16 - Pequeño gran héroe.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/01 - Hambruna.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/02 - Lejos estoy.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/03 - Canción para mi muerte.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/04 - Destino.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/05 - Mi marca.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/06 - Surfer's punk's (aquaman).mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/07 - No me moleste.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/08 - In the Street.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/09 - Ska de la rutina.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/10 - Casa de dos.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/11 - Correo de amor.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/12 - Naif.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/13 - Perdiendo el tiempo.mp3
+/home/ale/Música/2'/1999 - Dos Minutos de Advertencia/14 - Carta a E.T_.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/01 - Vos no confiaste.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/02 - Todo lo miro.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/03 - Novedades.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/04 - Amor suicida.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/05 - Ya no sos igual.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/06 - Canción de amor.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/07 - Copetín al paso.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/08 - Odio laborar.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/09 - Piñas van, piñas vienen.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/10 - Tema de Adrián.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/11 - Valentín Alsina.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/12 - Otra mujer.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/13 - Pelea callejera.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/14 - Vago.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/15 - Otra vez (La casa de Juan).mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/16 - El mejor recuerdo.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/17 - Borracho y agresivo.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/18 - Qué mala suerte.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/19 - Laburantes.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/20 - Q.E.P.D_.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/21 - Arrebato.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/22 - Como caramelo de limón.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/23 - Mosca de bar.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/24 - Ataque.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/25 - Barricada.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/26 - Demasiado tarde (La marcha).mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/27 - Domingos.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/28 - Improvisación.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/29 - Él se va.mp3
+/home/ale/Música/2'/1999 - 2 Minutos - Novedades/30 - Murga final - 2 Minutos un sentimiento.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/01 - Realidad uno.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/02 - Lado oscuro.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/03 - Jason.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/04 - Disneylandia.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/05 - Mundo para dos.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/06 - Sonrisa de papel.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/07 - Vibrador.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/08 - Siempre es lo mismo.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/09 - Pobre Susana.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/10 - Cargo de conciencia.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/11 - Vomito de bar.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/12 - Marilyn.mp3
+/home/ale/Música/2'/2000 - 2 Minutos -  Antorchas/13 - Veinte (2x10).mp3
+/home/ale/Música/2'/2002 - Vida Monótona/01 - Amor suicida.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/02 - Aquaman.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/03 - Vomito bar.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/04 - Lejos estoy.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/05 - Pelea callejera.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/06 - Lado oscuro.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/07 - Mosca de bar.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/08 - Otra mujer.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/09 - Todo lo miro.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/10 - No me moleste.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/11 - Destino.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/12 - Jason.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/13 - La balanza.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/14 - Perdiendo el tiempo.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/15 - La marcha.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/16 - Casa de dos.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/17 - Correo de amor.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/18 - El mejor recuerdo.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/19 - Tema de Adrian.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/20 - Realidad uno.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/21 - Gatillo fácil.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/22 - Casa de Juan.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/23 - NAIF.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/24 - Piñas van, piñas vienen.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/25 - Ya no sos igual.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/26 - Como caramelo de limón.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/27 - Barricada.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/28 - I Turned Into a Martian.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/29 - Arrebato.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/30 - Mala suerte.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/31 - Canción de amor.mp3
+/home/ale/Música/2'/2002 - Vida Monótona/32 - Nada que hacer.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/01 - Mundo TV.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/02 - Historia triste.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/03 - Corazón.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/04 - Angela.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/05 - Washington.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/06 - Puntero.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/07 - Vampira.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/08 - Ticket.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/09 - Qué más.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/10 - Tiempo.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/11 - Ezeiza.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/12 - Falta.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/13 - Cristina.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/14 - Piso 22.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/15 - Felicidad.mp3
+/home/ale/Música/2'/2004 - 2 Minutos - Superocho/16 - David Song's.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/01 - 2 minutos.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/02 - Cacería.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/03 - Pandora Box.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/04 - Un sueño.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/05 - Fotos rosadas.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/06 - Las piedras rodantes.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/07 - Aeropuerto.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/08 - El bondi.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/09 - Planetas.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/10 - Miss Muerte.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/11 - Soledad.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/12 - Somos más americanos.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/13 - Niño.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/14 - Cero.mp3
+/home/ale/Música/2'/2006 - 2 Minutos - Un Mundo de Sensaciones/15 - Rompan todo.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/01- Valentin Alsina.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/02 - Historia Triste.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/03 - Mosca De Bar.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/04 - No Me Molesten.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/05 - Tema De Adrian.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/06 - El Se Va.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/07 - Vampira.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/08 - Mala Suerte.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/09 - Corazón.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/10 - Naif.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/11 - Postal 95.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/12 - Que Yeta.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/13 - Cero.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/14 -Todo Lo Miro.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/15 - 14 Botellas.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/16 - Vos No Confiaste.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/17 - Demasiado Tarde (La Marcha).mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/18 - El Bondi.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/19 - Amor Suicida.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/20 - Destino (Nena).mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/33 - Washington.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/34 - Barricada.mp3
+/home/ale/Música/2'/2007 - 2 Minutos - 20 Años No Es Nada/35 - 2 Minutos.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/01 - Panik Atack.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/02 - Cabeza de limón.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/03 - Especial.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/04 - Navaja.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/05 - Resfriada.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/06 - Melodía de barrio.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/07 - A.X.E. (Amnesia por ebriedad).mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/08 - Paro general.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/09 - Metal marciano rojo.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/10 - Todavía una canción de amor.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/11 - 900 muy bien.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/12 - Fantasma.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/13 - El que no salta es un skon.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/14 - Ave de paso.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/15 - El asador.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/16 - Guitarrita y Fafa.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/17 - Esto no es el Italpark (La caprichosa).mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/18 - Maravillosa mente.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/19 - Efedrina.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/20 - Straight to Hell.mp3
+/home/ale/Música/2'/2010 - 2 Minutos - Vamos A La Granja_Directo Al Infierno/21 - Vamos a la granja.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/01 Gizmo.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/02 Hacia Vos.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/03 Regalo.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/04 Ya Perdi.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/05 Venus.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/06 Personal.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/07 Dias.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/08 Hay De Todo Menos Calma.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/09 Nadie.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/10 Tsunami.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/11 Ariel La Sirena.mp3
+/home/ale/Música/2'/2013 - Valentin Alzheimer/12 Catastrofe.mp3
+/home/ale/Música/2'/dos minutos - cancion de amor.mp3
+/home/ale/Música/2'/2 minutos - cancion para mi muerte.mp3
+/home/ale/Música/2'/dos minutos - enamorado de vos.mp3
+/home/ale/Música/2'/dos minutos - la ladrona.mp3
+/home/ale/Música/2'/dos minutos - ya no sos igual.mp3
+/home/ale/Música/2'/2 minutos - yo no se porque.mp3
+/home/ale/Música/2'/dos minutos - antorchas - un mundo para dos.mp3
+/home/ale/Música/2'/dos minutos - disney landia.mp3
+/home/ale/Música/2'/dos minutos - valentin alsina - amor suicida.mp3
+/home/ale/Música/2'/dos minutos - botellas.mp3
+/home/ale/Música/2'/dos minutos - valentin alsina - como caramelo de limon.mp3
+/home/ale/Música/2'/dos minutos - valentin alsina - arrebato.mp3
+/home/ale/Música/2'/dos minutos - a veces.mp3
+/home/ale/Música/2'/02-dos_minutos-historia_triste.mp3
+/home/ale/Música/2'/dos minutos - superocho - tiempo.mp3
+/home/ale/Música/2'/07-dos_minutos-vampira.mp3
+/home/ale/Música/2'/dos minutos y flema - hay una bomba en el colegio.mp3
